de0d15102e27e168d17ecf0b236cfa8787adab12
ZackMisso <zack441@mac.com>
2021-01-14 17:06:44 -0500

    some scripts for my current paper

--- bias/pixel_wise_mse.cpp
+++ bias/pixel_wise_mse.cpp
@@ -5,6 +5,41 @@
 #include "imedit/image.h"
 #include "imedit/im_util.h"
 
+double avg(double x,
+           double y,
+           double z)
+{
+    return (x + y + z) / 3.0;
+}
+
+double norm(double x,
+            double y,
+            double z)
+{
+    return std::sqrt(x*x + y*y + z*z);
+}
+
+double sqr_norm(double x,
+                double y,
+                double z)
+{
+    return x*x + y*y + z*z;
+}
+
+double max(double x,
+           double y,
+           double z)
+{
+    return std::max(x, std::max(y, z));
+}
+
+double min(double x,
+           double y,
+           double z)
+{
+    return std::min(x, std::min(y, z));
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 5)
@@ -24,16 +59,16 @@ int main(int argc, char* argv[])
     int height = gt_image.height();
 
     // create the difference images
-    for (int i = start; i < end; ++i)
-    {
-        imedit::Image inp1 = imedit::Image(im_directory + std::to_string(i+1) + ".exr");
-        imedit::Image in = imedit::Image(im_directory + std::to_string(i) + ".exr");
-
-        imedit::Image diff = inp1-in;
-        imedit::im_abs(diff);
-
-        diff.write(im_directory + std::to_string(i) + "_" + std::to_string(i+1) + ".exr");
-    }
+    // for (int i = start; i < end; ++i)
+    // {
+    //     imedit::Image inp1 = imedit::Image(im_directory + std::to_string(i+1) + ".exr");
+    //     imedit::Image in = imedit::Image(im_directory + std::to_string(i) + ".exr");
+    //
+    //     imedit::Image diff = inp1-in;
+    //     imedit::im_abs(diff);
+    //
+    //     diff.write(im_directory + std::to_string(i) + "_" + std::to_string(i+1) + ".exr");
+    // }
 
     // create actual data
     std::ofstream file;
@@ -44,10 +79,21 @@ int main(int argc, char* argv[])
     file << end << std::endl;
 
     std::vector<imedit::Image> images = std::vector<imedit::Image>();
+    std::vector<imedit::Image> var_images = std::vector<imedit::Image>();
 
     for (int k = start; k <= end; ++k)
     {
         images.push_back(imedit::Image(im_directory + std::to_string(k) + ".exr"));
+    }
+
+    for (int k = start; k < end; ++k)
+    {
+        std::cout << "PARSING: " << im_directory + "var_" +
+                                           std::to_string(k) + "_" +
+                                           std::to_string(k+1) + ".exr" << std::endl;
+        var_images.push_back(imedit::Image(im_directory + "var_" +
+                                           std::to_string(k) + "_" +
+                                           std::to_string(k+1) + ".txt"));
     }
 
     for (int i = 0; i < height; ++i)
@@ -59,14 +105,45 @@ int main(int argc, char* argv[])
 
             for (int k = 0; k < end - start; ++k)
             {
-                // take the average of the differences per pixel channel
-                double diff = std::abs(images[k+1](j, i, 0) - images[k](j, i, 0)) +
-                              std::abs(images[k+1](j, i, 1) - images[k](j, i, 1)) +
-                              std::abs(images[k+1](j, i, 2) - images[k](j, i, 2));
-                // diff /= 3.0;
+                // take the norm of the differences per pixel channel
+                double diff_x = std::abs(images[k+1](j, i, 0) - images[k](j, i, 0));
+                double diff_y = std::abs(images[k+1](j, i, 1) - images[k](j, i, 1));
+                double diff_z = std::abs(images[k+1](j, i, 2) - images[k](j, i, 2));
 
-                diffs.push_back(diff);
-                sum += diff;
+                double expected_qr = max(diff_x*diff_x,
+                                         diff_y*diff_y,
+                                         diff_z*diff_z);
+
+                double var_x = var_images[k](j, i, 0);
+                double var_y = var_images[k](j, i, 0);
+                double var_z = var_images[k](j, i, 0);
+
+                double var = max(var_x, var_y, var_z);
+
+                if (isinf(var) || isnan(var))
+                {
+                    var = 0.0;
+                }
+
+                double pmf_i = std::sqrt(var + expected_qr);
+
+                // var + I^2
+                diffs.push_back(pmf_i);
+
+                // if (i == 0 && j == 0)
+                // {
+                //     std::cout << "k: " << k << std::endl;
+                //     std::cout << "k: " << k << " EXPECTED: " << expected_qr << std::endl;
+                //     std::cout << "k: " << k << " Var: " << var << std::endl;
+                //     std::cout << "var_x: " << var_x << std::endl;
+                //     std::cout << "var_y: " << var_y << std::endl;
+                //     std::cout << "var_z: " << var_z << std::endl;
+                //
+                //     // var_images[k].write("wtf_man.exr");
+                //     // var_images[k].write("wtf_man.png");
+                // }
+
+                sum += pmf_i;
             }
 
             for (int k = 0; k < diffs.size(); ++k)
